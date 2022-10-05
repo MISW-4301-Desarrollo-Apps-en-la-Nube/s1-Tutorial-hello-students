9c5b3a70989a2827e0041bc0a41fd59d19ca7efb
andres.baron <andres.baron@rappi.com>
2022-10-05 17:59:33 -0500

    v0.0.2

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 
-import time
-from datetime import datetime
 
-print("Bienvenidos al curso desarrollo de aplicaciones nativas en la nube versión 0.0.3")
-while True:
-  now = datetime.now()
-  current_time = now.strftime("%H:%M:%S")
-  print(f"Consulta de logs contenedora versión 0.0.3 a las {current_time}")
-  time.sleep(2)
+print("Bienvenidos al curso desarrollo de aplicaciones nativas en la nube versión 0.0.2")
